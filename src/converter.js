8f4fe18c5924dc2e205f39b652e4d222f97ea26a
chrisdfghjkl <cdepastene@gmail.com>
2022-01-14 00:28:38 -0800

    add promise logic

---
+++ src/converter.js
@@ -0,0 +1,14 @@
+export default class GetConversion {  
+  static async getConversion(target, amount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${target}/${amount}
+      `);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
