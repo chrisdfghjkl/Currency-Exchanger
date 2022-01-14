55c88b1153f0cdb5f2f716a839c73b2109f53671
chrisdfghjkl <cdepastene@gmail.com>
2022-01-14 13:46:10 -0800

    add UI logic

--- src/index.js
+++ src/index.js
@@ -4,3 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import GetConversion from './converter'
 
+function getElements(response) {
+  if (response.result === "success") {
+    $(".showConversion").text(`the converted amount is ${response.conversion_result}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+
+async function makeApiCall(amount, target) {
+  const response = await GetConversion.getConversion(amount, target);
+  getElements(response);
+}
+
+$(document).ready(function() {
+  $('#convertSubmit').click(function() {
+    event.preventDefault();
+    let amount = $('#usd').val();
+    let target = $("select#intlCurrency").val(); 
+    makeApiCall(amount, target);
+  });
+});
