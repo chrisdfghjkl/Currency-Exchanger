3387a382ba4e0df57d3a9333611d1fc086d90f91
chrisdfghjkl <cdepastene@gmail.com>
2022-01-14 13:22:48 -0800

    edit function

--- src/converter.js
+++ src/converter.js
@@ -1,5 +1,5 @@
 export default class GetConversion {  
-  static async getConversion(target, amount) {
+  static async getConversion(amount, target) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${target}/${amount}
       `);
